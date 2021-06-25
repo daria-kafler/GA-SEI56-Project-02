9dbc8923aa3dfbb44beb3b1868f4de247bffad95
Daria Kafler <me@dariakafler.com>
2021-06-25 14:23:42 +0100

    chaneg html title section before deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  // const history = useHistory()
-  // const location = useLocation()
-
+  
   return (
     <nav className="navbar is-dark">
       <div className="container has-text-centered">
         <div className="navbar-brand">
           <span className="tag is-dark is-large has-text-light">
-            <Link to="/">❄️☀️ Start page ☀️❄️</Link>
+            <Link to="/">❄️ ☀️ Homepage ☀️ ❄️</Link>
           </span>
         </div>
       </div>
